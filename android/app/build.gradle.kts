3f3d5dcddd725c0bc1c6ba5672a163ba3249c273
AdonisCr <adonisoussou737@gmail.com>
2025-09-17 12:20:04 +0100

    correction config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,13 +11,13 @@ plugins {
 android {
     namespace = "com.example.rdv_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-    ndkVersion "27.0.12077973"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
